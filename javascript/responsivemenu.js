c701237e446c9eab5507350b7b2f78c5795e9eed
Kristian Bakov <Kristian.bakov@yahoo.com>
2022-06-23 15:57:13 +0100

    Added introduction section

---
+++ javascript/responsivemenu.js
@@ -0,0 +1,17 @@
+(function () {
+  const hamburgerMenu = document.querySelector("div.hamburger");
+  const respMenuElements = document.querySelectorAll(
+    "nav.header-nav, ul.nav-primary"
+  );
+
+  function toggleMenu(x) {
+    x.classList.toggle("change");
+  }
+
+  hamburgerMenu.addEventListener("click", () => {
+    toggleMenu(hamburgerMenu);
+    for (var i = 0; i < respMenuElements.length; i++) {
+      respMenuElements[i].classList.toggle("change");
+    }
+  });
+})();
